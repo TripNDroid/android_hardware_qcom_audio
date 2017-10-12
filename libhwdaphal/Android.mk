0f8d77eb88190fe74962fab419243d103a021285
TripNRaVeR <noerialexeew@live.nl>
2017-10-13 01:57:38 +0200

    msm8998: add libhwdaphal source

---
+++ libhwdaphal/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := src/dap_hal_api.c \
+                   src/dap_hal_util.c
+
+ifneq ($(filter apq8084,$(TARGET_BOARD_PLATFORM)),)
+  LOCAL_CFLAGS := -DPLATFORM_APQ8084
+endif
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    liblog \
+    libdl
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/inc \
+    $(TOPDIR)external/tinyalsa/include \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+    $(TARGET_OUT_INTERMEDIATES)/include/mm-audio/audio-effects/ds2
+
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_COPY_HEADERS_TO := mm-audio/audio-effects/ds2
+LOCAL_COPY_HEADERS := inc/dap_hal_api.h
+
+LOCAL_MODULE := libhwdaphal
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
